ae42ba4ee1e746f5f814ba22eb70ac23235b9e1e
ayun1998-3 <77704153+ayun1998-3@users.noreply.github.com>
2025-03-09 16:41:04 -0400

    Added acuatorControl functions
    Used code from linearAcutatorSyncing branch

---
+++ Core/Inc/actuatorControl.h
@@ -0,0 +1,30 @@
+/*
+ * actuatorControl
+ *
+ *  Created on: Mar 4, 2025
+ *      Author: rojo
+*/
+
+// Functions for synchronizing and controlling track actuators
+
+#ifndef __ACTUATOR_CONTROL_H
+#define __ACTUATOR_CONTROL_H
+
+#include "stm32f7xx_hal.h"
+#include "TalonSRX.h"
+
+#define TOLERANCE 1.004
+#define SLOWFACTOR 0.99
+
+
+// percent output values for left and right actuators
+struct ActuatorValues {
+  float left;
+  float right;
+};
+
+
+void setActuatorLength(TalonSRX leftActuator, TalonSRX rightActuator, float percentExtension);
+struct ActuatorValues syncLinearActuators(float percentOutput);
+
+#endif
---
+++ Core/Src/actuatorControl.c
@@ -0,0 +1,63 @@
+#include "actuatorControl.h"
+
+extern ADC_HandleTypeDef hadc1;
+extern ADC_HandleTypeDef hadc2;
+#define FULL_ADC_RANGE 4096 // todo: check if this value is right
+#define POSITION_TOLERANCE 10
+
+
+
+// sets actuator length for a length between 0 (fully retracted) and 1 (fully extended)
+void setActuatorLength(TalonSRX leftActuator, TalonSRX rightActuator, float percentExtension) {
+	// Get current length of actuators
+	  HAL_ADC_Start(&hadc1);
+	  HAL_ADC_Start(&hadc2);
+	  HAL_ADC_PollForConversion(&hadc1, 20);
+	  uint16_t leftPosition = HAL_ADC_GetValue(&hadc1); // Change to HAL_ADCEx_MultiModeStart_DMA or HAL_ADC_Start_DMA for speed
+	  HAL_ADC_PollForConversion(&hadc2, 20);
+	  uint16_t rightPosition = HAL_ADC_GetValue(&hadc2); // todo: also check rightPosition once we know this logic works.
+
+	  struct ActuatorValues percentOutputs;
+	  uint16_t targetPosition = percentExtension * FULL_ADC_RANGE;
+	  // if current actuator length is approximately equal to target length, set Talon SRX outputs to 0
+	  if (leftPosition < targetPosition + POSITION_TOLERANCE && leftPosition > targetPosition - POSITION_TOLERANCE) {
+		  percentOutputs = (struct ActuatorValues) {
+			.left = 0,
+			.right = 0,
+		  };
+	  }
+	  if (leftPosition < targetPosition) {
+		  percentOutputs = syncLinearActuators(1); // extend actuators at full speed
+	  }
+	  else if (leftPosition < percentExtension * FULL_ADC_RANGE) {
+		  percentOutputs = syncLinearActuators(-1); // retract actuators at full speed
+	  }
+	  leftActuator.set(&leftActuator, percentOutputs.left);
+	  rightActuator.set(&rightActuator, percentOutputs.right);
+}
+
+// given target actuator speed, use potentiometer feedback to ensure actuators are at approximately the same length of extension
+// if one actuator is ahead of the other, slow down the faster actuator by a set speed
+// returns percent outputs of the TalonSRXs controlling the actuators
+struct ActuatorValues syncLinearActuators(float percentOutput) {
+  HAL_ADC_Start(&hadc1);
+  HAL_ADC_Start(&hadc2);
+
+  HAL_ADC_PollForConversion(&hadc1, 20);
+  uint16_t leftPosition = HAL_ADC_GetValue(&hadc1); // Change to HAL_ADCEx_MultiModeStart_DMA or HAL_ADC_Start_DMA for speed
+
+  HAL_ADC_PollForConversion(&hadc2, 20);
+  uint16_t rightPosition = HAL_ADC_GetValue(&hadc2);
+
+  struct ActuatorValues newPercentOutputs;
+  newPercentOutputs.left = percentOutput;
+  newPercentOutputs.right = percentOutput;
+
+  // if difference between left and right actuator lengths are larger than a certain tolerance, slow down faster actuator
+  if ((leftPosition > rightPosition * TOLERANCE && percentOutput > 0) || (leftPosition < rightPosition * TOLERANCE && percentOutput < 0)) {
+    newPercentOutputs.left *= SLOWFACTOR;
+  } else if ((rightPosition > leftPosition * TOLERANCE && percentOutput > 0) || (rightPosition < leftPosition * TOLERANCE && percentOutput < 0)) {
+    newPercentOutputs.right *= SLOWFACTOR;
+  }
+  return newPercentOutputs;
+}
