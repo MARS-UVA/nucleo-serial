79be8400d56495205c6cae5aa6c75f29fccd5924
Diana Lin <lindiana1206@gmail.com>
2025-03-11 18:35:04 -0400

    remove unused pdp function

--- Core/Src/util.c
+++ Core/Src/util.c
@@ -21,32 +21,6 @@ void sendCANMessage(CAN_HandleTypeDef *hcan, int identifier, char *message, uint
 		Error_Handler();
 }
 
- pdpGetCurrentReadings(CAN_HandleTypeDef *hcan, int pdpIdentifier, float *readings)
-{
-	sendCANMessage(hcan, pdpIdentifier, "\x00\x00\x00\x00\x20\x00", 6);
-	HAL_Delay(1);
-
-	CAN_RxHeaderTypeDef hdr;
-	char data[8];
-	HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &hdr, data);
-
-	float val = ((data[5] << 16) + (data[6] << 8) + data[7]) * 0.125;
-
-	for (int i = 0; i < 3; i++)
-	{
-		switch (hdr.ExtId)
-		{
-		case 0x8041400:
-
-			break;
-		case 0x8041440:
-			break;
-		case 0x8041480:
-			break;
-		}
-	}
-}
-
 void floatToByteArray(float f, char *arr)
 {
     unsigned int asInt = *((int*) &f);
